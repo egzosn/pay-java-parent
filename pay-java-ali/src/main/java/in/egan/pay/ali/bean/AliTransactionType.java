490d99a84d999d5fdafb11bb9ca7f7d8798538b4
cnzzs <cnzhengzs@gmail.com>
2016-10-21 00:09:59 +0800

    整理支付，新增微信扫码支付

--- pay-java-ali/src/main/java/in/egan/pay/ali/bean/AliTransactionType.java
+++ pay-java-ali/src/main/java/in/egan/pay/ali/bean/AliTransactionType.java
@@ -8,8 +8,8 @@ import in.egan.pay.common.bean.TransactionType;
  * @date 2016/10/19 22:58
  */
 public enum  AliTransactionType implements TransactionType {
-
-     NATIVE("create_direct_pay_by_user"),APP("mobile.securitypay.pay");
+    //即时到帐                          //移动支付                      //手机网站支付
+    DIRECT("create_direct_pay_by_user"),APP("mobile.securitypay.pay"),WAP("alipay.wap.create.direct.pay.by.user");
 
     private String type;
 
