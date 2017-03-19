4836e552f73703163c2ae2583eb8996ee9374b5c
egzosn <egzosn@gmail.com>
2017-03-19 23:32:24 +0800

    重新定义二维码支付接口

--- pay-java-common/src/main/java/in/egan/pay/common/http/UriVariables.java
+++ pay-java-common/src/main/java/in/egan/pay/common/http/UriVariables.java
@@ -1,6 +1,14 @@
 package in.egan.pay.common.http;
 
+import com.alibaba.fastjson.JSON;
+import org.apache.http.Consts;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -39,6 +47,8 @@ public class UriVariables {
         return uri;
     }
 
+
+
     /**
      * 匹配Map.key
      * @param uri
@@ -68,6 +78,53 @@ public class UriVariables {
         }
         return uri;
     }
+
+
+
+    /**
+     * Map转化为对应得参数字符串
+     * @param pe
+     * @return
+     */
+    public static String getMapToParameters(Map pe){
+        StringBuilder builder = new StringBuilder();
+        for (Object key : pe.keySet()) {
+            Object o = pe.get(key);
+
+            if (null == o) {
+                continue;
+            }
+
+            if (o instanceof List) {
+                o = ((List) o).toArray();
+            }
+            try {
+                if (o instanceof Object[]) {
+                    Object[] os = (Object[]) o;
+                    String valueStr = "";
+                    for (int i = 0, len = os.length; i < len; i++) {
+                        if (null == os[i]) {
+                            continue;
+                        }
+                        String value = os[i].toString().trim();
+                        valueStr += (i == len - 1) ?  value :  value + ",";
+                    }
+                    builder.append(key).append("=").append(URLEncoder.encode(valueStr, "utf-8")).append("&");
+
+                    continue;
+                }
+                builder.append(key).append("=").append(URLEncoder.encode((String) pe.get(key), "utf-8")).append("&");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        if (builder.length() > 1) {
+            builder.deleteCharAt(builder.length() - 1);
+        }
+        return builder.toString();
+    }
+
+
 
 
 }
