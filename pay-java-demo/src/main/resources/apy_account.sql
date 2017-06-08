7193e4d3d3e805c06bade2d98811d02a032ef754
egzosn@gmail.com <930928lI1>
2017-06-08 15:40:12 +0800

    库表更新

--- pay-java-demo/src/main/resources/apy_account.sql
+++ pay-java-demo/src/main/resources/apy_account.sql
@@ -1,25 +1,26 @@
 
 /*Table structure for table `apy_account` */
 
-DROP TABLE IF EXISTS `apy_account`;
+DROP TABLE IF EXISTS `pay_account`;
 
-CREATE TABLE `apy_account` (
-  `pay_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '支付账号id',
-  `partner` varchar(32) DEFAULT NULL COMMENT '支付合作id,商户id，差不多是支付平台的账号或id',
-  `appid` varchar(32) DEFAULT NULL COMMENT '应用id',
-  `public_key` varchar(1204) DEFAULT NULL COMMENT '支付公钥，sign_type只有单一key时public_key与private_key相等，比如sign_type=MD5的情况',
-  `private_key` varchar(2048) DEFAULT NULL COMMENT '支付私钥',
-  `notify_url` varchar(1024) DEFAULT NULL COMMENT '异步回调地址',
-  `return_url` varchar(1024) DEFAULT NULL COMMENT '同步回调地址',
-  `seller` varchar(256) DEFAULT NULL COMMENT '收款账号, 针对支付宝',
-  `sign_type` varchar(16) DEFAULT NULL COMMENT '签名类型',
-  `input_charset` varchar(16) DEFAULT NULL COMMENT '枚举值，字符编码 utf-8,gbk等等',
-  `pay_type` char(16) DEFAULT NULL COMMENT '支付类型,aliPay：支付宝，wxPay：微信, youdianPay: 友店微信,此处开发者自定义对应com.egzosn.pay.demo.entity.PayType枚举值',
-  `msg_type` char(8) DEFAULT NULL COMMENT '消息类型，text,xml,json',
-  `create_by` char(32) DEFAULT NULL COMMENT '创建人',
-  `create_time` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+CREATE TABLE `pay_account` (
+  `pay_id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '支付账号id',
+  `partner` VARCHAR(32) DEFAULT NULL COMMENT '支付合作id,商户id，差不多是支付平台的账号或id',
+  `appid` VARCHAR(32) DEFAULT NULL COMMENT '应用id',
+  `public_key` VARCHAR(1204) DEFAULT NULL COMMENT '支付公钥，sign_type只有单一key时public_key与private_key相等，比如sign_type=MD5的情况',
+  `private_key` VARCHAR(2048) DEFAULT NULL COMMENT '支付私钥',
+  `notify_url` VARCHAR(1024) DEFAULT NULL COMMENT '异步回调地址',
+  `return_url` VARCHAR(1024) DEFAULT NULL COMMENT '同步回调地址',
+  `seller` VARCHAR(256) DEFAULT NULL COMMENT '收款账号, 针对支付宝',
+  `sign_type` VARCHAR(16) DEFAULT NULL COMMENT '签名类型',
+  `input_charset` VARCHAR(16) DEFAULT NULL COMMENT '枚举值，字符编码 utf-8,gbk等等',
+  `pay_type` CHAR(16) DEFAULT NULL COMMENT '支付类型,aliPay：支付宝，wxPay：微信, youdianPay: 友店微信,此处开发者自定义对应com.egzosn.pay.demo.entity.PayType枚举值',
+  `msg_type` CHAR(8) DEFAULT NULL COMMENT '消息类型，text,xml,json',
+  `is_test` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '是否为测试环境',
+  `create_by` CHAR(32) DEFAULT NULL COMMENT '创建人',
+  `create_time` TIMESTAMP NULL DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`pay_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 /*Data for the table `apy_account` */
 
