3639ab0804a5e87ce2136c4db7a698ea8e76830e
egzosn@gmail.com <930928lI1>
2017-06-15 10:37:59 +0800

    银联支付初始化

---
+++ pay-java-union/src/main/java/com/egzosn/pay/union/api/UnionPayConfigStorage.java
@@ -0,0 +1,38 @@
+package com.egzosn.pay.union.api;
+
+import com.egzosn.pay.common.api.BasePayConfigStorage;
+
+/**
+ * 银联支付配置
+ * @author egan
+ *
+ *
+ * email egzosn@gmail.com
+ * date 2017/6/15
+ */
+public class UnionPayConfigStorage extends BasePayConfigStorage {
+    @Override
+    public String getAppid() {
+        return null;
+    }
+
+    @Override
+    public String getPartner() {
+        return null;
+    }
+
+    @Override
+    public String getPid() {
+        return null;
+    }
+
+    @Override
+    public String getSeller() {
+        return null;
+    }
+
+    @Override
+    public String getSecretKey() {
+        return null;
+    }
+}
