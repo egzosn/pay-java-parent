5b9e2a81f3c70991fbc1169c674934be36d95603
egzosn <egzosn@gmail.com>
2017-11-14 14:50:16 +0800

    代码优化，完善帮助文档，对H5支付开放ip与网页连接等参数

--- pay-java-wx/src/main/java/com/egzosn/pay/wx/bean/WxTransactionType.java
+++ pay-java-wx/src/main/java/com/egzosn/pay/wx/bean/WxTransactionType.java
@@ -10,28 +10,47 @@ import com.egzosn.pay.common.bean.TransactionType;
  * date 2016/10/19 22:58
  */
 public enum  WxTransactionType implements TransactionType {
-    //公众号支付
+    /**
+     * 公众号支付
+     */
     JSAPI("pay/unifiedorder"),
-    //扫码付
+    /**
+     * 扫码付
+     */
     NATIVE("pay/unifiedorder"),
-    //移动支付
+    /**
+     * 移动支付
+     */
     APP("pay/unifiedorder"),
+    /**
+     * H5支付
+     */
     MWEB("pay/unifiedorder"),
-    //刷卡付，暂未接触
+    /**
+     * 刷卡付
+     */
     MICROPAY("pay/micropay"),
     // TODO 2017/3/8 19:14 author: egan  交易辅助接口
-    //查询订单
+    /**
+     * 查询订单
+     */
     QUERY("pay/orderquery"),
-    //关闭订单
+    /**
+     * 关闭订单
+     */
     CLOSE("pay/closeorder"),
-    //申请退款
+    /**
+     * 申请退款
+     */
     REFUND("secapi/pay/refund"),
-    //查询退款
+    /**
+     * 查询退款
+     */
     REFUNDQUERY("pay/refundquery"),
-    //下载对账单
-    DOWNLOADBILL("pay/downloadbill"),
-    //不知道交易类型，主要用于回调通知，回调后不清楚交易类型，以此定义
-    UNAWARE("UNAWARE")
+    /**
+     * 下载对账单
+     */
+    DOWNLOADBILL("pay/downloadbill")
     ;
 
     WxTransactionType(String method) {
