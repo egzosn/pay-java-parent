17edc27fc46ebafcca2e78aac95f0b85e8fbd76c
egzosn <egzosn@gmail.com>
2017-11-22 12:27:59 +0800

    移除测试代码

--- pay-java-wx/src/main/java/com/egzosn/pay/wx/api/WxPayConfigStorage.java
+++ pay-java-wx/src/main/java/com/egzosn/pay/wx/api/WxPayConfigStorage.java
@@ -14,7 +14,7 @@ import com.egzosn.pay.common.api.BasePayConfigStorage;
 public class WxPayConfigStorage extends BasePayConfigStorage {
 
 
-    /*
+    /**
      * 应用id
      */
     private   String appid ;
