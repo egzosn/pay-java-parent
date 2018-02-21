581def678ed2374bb6e66fcac45e3715a0d20382
egan <egzosn@gmail.com>
2018-02-22 00:05:43 +0800

    优化交易辅助接口

--- pay-java-wx-youdian/src/main/java/com/egzosn/pay/wx/youdian/bean/YoudianTransactionType.java
+++ pay-java-wx-youdian/src/main/java/com/egzosn/pay/wx/youdian/bean/YoudianTransactionType.java
@@ -12,13 +12,36 @@ import com.egzosn.pay.common.bean.TransactionType;
 public enum  YoudianTransactionType implements TransactionType {
 
     /**
+     * 登录获取授权码
+     */
+    LOGIN("login"),
+    /**
+     * 刷新授权码
+     */
+    RESET_LOGIN("resetLogin"),
+    /**
      * 扫码付
      */
     NATIVE("unifiedorder"),
     /**
+     * 查看扫码付款订单状态
+     */
+    NATIVE_STATUS("unifiedorderStatus"),
+
+    /**
      * 刷卡付
      */
-    MICROPAY("micropay");//暂未接触
+    MICROPAY("micropay"),
+    /**
+     * 查看刷卡付款订单状态
+     */
+    MICROPAY_STATUS("unifiedorderStatus"),
+
+    /**
+     * 退款
+     */
+    REFUND("orderRefund")
+    ;
 
     private String method;
 
