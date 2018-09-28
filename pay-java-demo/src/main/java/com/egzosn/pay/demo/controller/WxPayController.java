54485c038d81a4322c733c3a2749cabf349a0cf0
egan <egzosn@gmail.com>
2018-09-28 21:07:49 +0800

    微信转账demo实现

--- pay-java-demo/src/main/java/com/egzosn/pay/demo/controller/WxPayController.java
+++ pay-java-demo/src/main/java/com/egzosn/pay/demo/controller/WxPayController.java
@@ -283,7 +283,7 @@ public class WxPayController {
 
 
     /**
-     * 转账
+     * 转账到余额
      *
      * @param order 转账订单
      *
@@ -292,15 +292,36 @@ public class WxPayController {
     @RequestMapping("transfer")
     public Map<String, Object> transfer(TransferOrder order) {
         order.setOutNo("partner_trade_no 商户转账订单号");
+        order.setPayeeAccount("用户openid");
+        order.setPayeeName("收款用户姓名， 非必填，如果填写将强制验证收款人姓名");
+        order.setRemark("转账备注, 非必填");
+        order.setAmount(new BigDecimal(10));
+
+        //转账到余额，这里默认值是转账到银行卡
+        order.setTransferType(WxTransferType.TRANSFERS);
+
+        return service.transfer(order);
+    }
+
+
+    /**
+     * 转账到银行卡
+     *
+     * @param order 转账订单
+     *
+     * @return 对应的转账结果
+     */
+    @RequestMapping("transfer")
+    public Map<String, Object> transferPayBank(TransferOrder order) {
+        order.setOutNo("partner_trade_no 商户转账订单号");
         //采用标准RSA算法，公钥由微信侧提供,将公钥信息配置在PayConfigStorage#setKeyPublic(String)
         order.setPayeeAccount("enc_bank_no 收款方银行卡号");
         order.setPayeeName("收款方用户名");
         order.setBank(WxBank.ABC);
         order.setRemark("转账备注, 非必填");
         order.setAmount(new BigDecimal(10));
-
-        //转账到余额，这里默认值是转账到银行卡
-        order.setTransferType(WxTransferType.TRANSFERS);
+        //转账到银行卡，这里默认值是转账到银行卡
+        order.setTransferType(WxTransferType.PAY_BANK);
 
         return service.transfer(order);
     }
