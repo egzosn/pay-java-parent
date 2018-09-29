697396533b512108cbf578aa47e6fd2810de0a76
egan <egzosn@gmail.com>
2018-09-29 18:06:45 +0800

    将证书等设置方式分开，便于使用者更容易使用

--- pay-java-union/src/main/java/com/egzosn/pay/union/api/UnionPayConfigStorage.java
+++ pay-java-union/src/main/java/com/egzosn/pay/union/api/UnionPayConfigStorage.java
@@ -19,8 +19,6 @@ public class UnionPayConfigStorage extends BasePayConfigStorage {
      */
     private volatile String merId;
 
-
-
     /**
      * 商户收款账号
      */
@@ -34,19 +32,89 @@ public class UnionPayConfigStorage extends BasePayConfigStorage {
      */
     private volatile String accessType = "0";
 
+    /**
+     * 中级证书路径
+     */
+    private String acpMiddleCert;
+    /**
+     * 根证书路径
+     */
+    private String acpRootCert;
 
+    /**
+     * 私钥证书是否已经初始化
+     *  默认没有
+     */
+    private boolean keyPrivateInit = false;
+
+    /**
+     * 公钥证书是否已经初始化
+     *  默认没有
+     */
+    private boolean keyPublicInit = false;
+
+
+    /**
+     * 设置私钥证书
+     * @param certificatePath 私钥证书地址
+     *  私钥证书密码 {@link #setKeyPrivateCertPwd(String)}
+     */
+    public void setKeyPrivateCert(String certificatePath){
+        super.setKeyPrivate(certificatePath);
+    }
+
+    /**
+     * 设置中级证书
+     * @param certificatePath 证书地址
+     */
+    public void setAcpMiddleCert(String certificatePath){
+        this.acpMiddleCert = certificatePath;
+    }
+    /**
+     * 设置根证书路径
+     * @param certificatePath 证书路径
+     */
+    public void setAcpRootCert(String certificatePath){
+        this.acpRootCert = certificatePath;
+    }
+
+    public String getAcpMiddleCert() {
+        return acpMiddleCert;
+    }
+
+    public String getAcpRootCert() {
+        return acpRootCert;
+    }
+
+    /**
+     *
+     *  设置私钥证书与证书密码
+     * @param keyPrivate 私钥证书与证书对应的密码 格式: D:/certs/acp_test_sign.pfx;000000
+     *  替代方法
+     * {@link #setKeyPrivateCert(String)}
+     * {@link #setKeyPrivateCertPwd(String)}
+     */
+    @Deprecated
     @Override
     public void setKeyPrivate(String keyPrivate) {
         super.setKeyPrivate(keyPrivate);
         if (isCertSign() && keyPrivate.length() < 1024 && keyPrivate.contains(";")){
             String[] split = keyPrivate.split(";");
-            setKeyPrivateCertPwd( split[1]);
+            super.setKeyPrivateCertPwd( split[1]);
             super.setKeyPrivate(split[0]);
             getCertDescriptor().initPrivateSignCert(getKeyPrivate(), getKeyPrivateCertPwd(), "PKCS12");
+            keyPrivateInit = true;
         }
     }
 
-
+    /**
+     * 设置中级证书与根证书  格式：D:/certs/acp_test_middle.cer;D:/certs/acp_test_root.cer
+     * @param keyPublic 中级证书与根证书
+     *  替代方法
+     * {@link #setAcpRootCert(String)}
+     * {@link #setAcpMiddleCert(String)}
+     */
+    @Deprecated
     @Override
     public void setKeyPublic(String keyPublic) {
         super.setKeyPublic(keyPublic);
@@ -54,11 +122,12 @@ public class UnionPayConfigStorage extends BasePayConfigStorage {
             String[] split = keyPublic.split(";");
             getCertDescriptor().initPublicCert(split[0]);
             getCertDescriptor().initRootCert(split[1]);
+            keyPublicInit = true;
         }
     }
 
     @Override
-    public String getAppid () {
+    public String getAppid() {
         return null;
     }
 
@@ -67,7 +136,7 @@ public class UnionPayConfigStorage extends BasePayConfigStorage {
      * @see #getPid()
      */
     @Deprecated
-    public String getPartner () {
+    public String getPartner() {
         return merId;
     }
 
@@ -79,12 +148,12 @@ public class UnionPayConfigStorage extends BasePayConfigStorage {
      * @see #setPid(String)
      */
     @Deprecated
-    public void setPartner (String partner) {
+    public void setPartner(String partner) {
         this.merId = partner;
     }
 
     @Override
-    public String getPid () {
+    public String getPid() {
         return merId;
     }
 
@@ -92,7 +161,7 @@ public class UnionPayConfigStorage extends BasePayConfigStorage {
         this.merId = pid;
     }
     @Override
-    public String getSeller () {
+    public String getSeller() {
         return seller;
     }
 
@@ -100,7 +169,7 @@ public class UnionPayConfigStorage extends BasePayConfigStorage {
         this.seller = seller;
     }
 
-    public String getMerId () {
+    public String getMerId() {
         return merId;
     }
 
@@ -122,5 +191,13 @@ public class UnionPayConfigStorage extends BasePayConfigStorage {
 
     public void setAccessType(String accessType) {
         this.accessType = accessType;
+    }
+
+    public boolean isKeyPrivateInit() {
+        return keyPrivateInit;
+    }
+
+    public boolean isKeyPublicInit() {
+        return keyPublicInit;
     }
 }
