f586c6f24c01fe8a86ab7769e9dcacd75b1c7734
egzosn <930928.lI1>
2020-05-24 23:41:47 +0800

    转账订单设置属性信息

--- pay-java-common/src/main/java/com/egzosn/pay/common/bean/TransferType.java
+++ pay-java-common/src/main/java/com/egzosn/pay/common/bean/TransferType.java
@@ -1,5 +1,7 @@
 package com.egzosn.pay.common.bean;
 
+import java.util.Map;
+
 /**
  * 转账类型
  * @author egan
@@ -7,6 +9,12 @@ package com.egzosn.pay.common.bean;
  *         date 2018/9/28.19:45
  */
 public interface TransferType extends TransactionType{
-
-
+    /**
+     * 设置属性
+     *
+     * @param attr 已有属性对象
+     * @param order 转账订单
+     * @return 属性对象
+     */
+    Map<String, Object> setAttr(Map<String, Object> attr, TransferOrder order);
 }
