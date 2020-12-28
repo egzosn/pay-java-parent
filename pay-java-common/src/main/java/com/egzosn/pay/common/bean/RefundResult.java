3805fcdc698c78ff37556f862cc1293116b05996
egzosn <egzosn@gmail.com>
2020-12-28 10:23:30 +0800

    退款序列号问题

--- pay-java-common/src/main/java/com/egzosn/pay/common/bean/RefundResult.java
+++ pay-java-common/src/main/java/com/egzosn/pay/common/bean/RefundResult.java
@@ -6,15 +6,16 @@ import java.util.Map;
 
 /**
  * 退款结果
- *
+ * <p>
  * 这里继承Map为兼容方案，后续版本进行删除
+ *
  * @author Egan
  * <pre>
  * email egzosn@gmail.com
  * date 2020/8/16 9:55
  * </pre>
  */
-public interface RefundResult extends Map<String, Object>, Serializable {
+public interface RefundResult extends Serializable {
     /**
      * 获取退款结果原信息集
      *
@@ -29,6 +30,7 @@ public interface RefundResult extends Map<String, Object>, Serializable {
      * @return 属性值
      */
     Object getAttr(String key);
+
     /**
      * 获取退款结果属性值
      *
@@ -36,6 +38,7 @@ public interface RefundResult extends Map<String, Object>, Serializable {
      * @return 属性值
      */
     String getAttrString(String key);
+
     /**
      * 获取退款结果属性值
      *
@@ -47,42 +50,49 @@ public interface RefundResult extends Map<String, Object>, Serializable {
 
     /**
      * 获取退款请求结果状态码
+     *
      * @return 状态码
      */
     String getCode();
 
     /**
      * 获取退款请求结果状态提示信息
+     *
      * @return 提示信息
      */
     String getMsg();
 
     /**
      * 返回业务结果状态码
+     *
      * @return 业务结果状态码
      */
     String getResultCode();
 
     /**
      * 返回业务结果状态提示信息
+     *
      * @return 业务结果状态提示信息
      */
     String getResultMsg();
 
     /**
      * 退款金额
+     *
      * @return 退款金额
      */
     BigDecimal getRefundFee();
+
     /**
      * 退款币种信息
+     *
      * @return 币种信息
      */
     CurType getRefundCurrency();
 
     /**
-     *  支付平台交易号
-     *  发起支付时 支付平台(如支付宝)返回的交易订单号
+     * 支付平台交易号
+     * 发起支付时 支付平台(如支付宝)返回的交易订单号
      *
      * @return 支付平台交易号
      */
@@ -91,12 +101,14 @@ public interface RefundResult extends Map<String, Object>, Serializable {
     /**
      * 支付订单号
      * 发起支付时，用户系统的订单号
+     *
      * @return 支付订单号
      */
     String getOutTradeNo();
 
     /**
      * 商户退款单号
+     *
      * @return 商户退款单号
      */
     String getRefundNo();
